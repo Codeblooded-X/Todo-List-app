433ceaf5c80a29bd97f90f451a807de5be9a87d3
Garg <hardik.garg@nokia.com>
2020-05-05 15:38:07 +0530

    Updates

---
+++ precache-manifest.7ac9190f29524e8e2d4c3c63b961b327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d464e3a4caf5bb31f72294aab482622",
+    "url": "/Todo-List-app/index.html"
+  },
+  {
+    "revision": "d6a93335c6fdf0eea1da",
+    "url": "/Todo-List-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "30b177a4059d0f8e2322",
+    "url": "/Todo-List-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d6a93335c6fdf0eea1da",
+    "url": "/Todo-List-app/static/js/2.b82c84b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List-app/static/js/2.b82c84b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b177a4059d0f8e2322",
+    "url": "/Todo-List-app/static/js/main.815dccb8.chunk.js"
+  },
+  {
+    "revision": "321047ea4a50924d659f",
+    "url": "/Todo-List-app/static/js/runtime-main.42c7ec71.js"
+  }
+]);
